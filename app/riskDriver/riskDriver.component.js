6df7029cc60bc04055db4f5ea161c4a0832c87d5
Victor Figueira <victor.sfl@gmail.com>
2016-11-07 09:47:39 +0000

    2.0.4
    fixing riskDriver Header and footer

--- app/riskDriver/riskDriver.component.js
+++ app/riskDriver/riskDriver.component.js
@@ -1,5 +1,10 @@
 
 function riskDriverController($scope, $http) {
+  $scope.selectedTab = 0;
+  $scope.getTab = function (item){
+    $scope.selectedTab = item;
+    console.log($scope.selectedTab );
+  }
 }
 
 angular.module('FqApp').component('chillRiskdriver', {
